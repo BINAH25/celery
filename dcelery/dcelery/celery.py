9e3189b72ca4bdd5db600a2c793c9dcf0880f61e
visteen <visteen192@gmail.com>
2023-09-16 12:13:56 +0000

    new task aeed

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -1,11 +1,8 @@
 import os
 from celery import Celery
+from django.conf import settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-@app.task
-def add_numbers():
-    return
-
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
